6bf552ae7656cc814e7c1d9eaba697c5219c31a4
Natalia <215136933+Nana-04-art@users.noreply.github.com>
2026-08-13 15:36:09 +0200

    code/beesagono_fe: implement Rules Component

---
+++ code/beesagono_fe/src/app/components/header/rules/rules.component.ts
@@ -0,0 +1,17 @@
+import { Component, output, signal } from '@angular/core';
+import { RANK_TIERS } from '../../../config/rank-tiers.config';
+
+@Component({
+  selector: 'app-rules',
+  imports: [],
+  templateUrl: './rules.component.html',
+  styleUrl: './rules.component.scss',
+})
+export class RulesComponent {
+  readonly rankTiers = RANK_TIERS;
+  readonly close = output<void>();
+
+  closeRules(): void {
+    this.close.emit();
+  }
+}
