2cbfeccd578e77ce45596f0ebe829d7f98192ebe
Natalia <215136933+Nana-04-art@users.noreply.github.com>
2026-08-25 15:11:40 +0200

    code/beesagono_fe: fix styles.scss and Game Service

--- code/beesagono_fe/src/app/services/game/game.service.ts
+++ code/beesagono_fe/src/app/services/game/game.service.ts
@@ -22,13 +22,6 @@ export function getTodayIsoString(date = new Date()): string {
     return `${year}-${month}-${day}`;
 }
 
-export function getTodayIsoString(date = new Date()): string {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-}
-
 @Injectable({
     providedIn: 'root',
 })
