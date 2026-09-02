9a00cde793d80d1ccd9074a18929d77884be5820
Natalia <215136933+Nana-04-art@users.noreply.github.com>
2026-09-02 15:13:55 +0200

    code/beesagono_be: implement dictionary management and word validation logic

--- code/beesagono_be/src/main/java/com/beesagono/backend/repository/DictionaryWordRepository.java
+++ code/beesagono_be/src/main/java/com/beesagono/backend/repository/DictionaryWordRepository.java
@@ -2,10 +2,13 @@ package com.beesagono.backend.repository;
 
 import com.beesagono.backend.entity.DictionaryWord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface DictionaryWordRepository extends JpaRepository<DictionaryWord, String> {
+public interface DictionaryWordRepository
+        extends JpaRepository<DictionaryWord, String>, JpaSpecificationExecutor<DictionaryWord> {
     Optional<DictionaryWord> findByWord(String word);
+
     Boolean existsByWord(String word);
 }
