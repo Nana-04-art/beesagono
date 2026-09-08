79d0e9ebae1b88fd84b773358f65493cab2c3b30
Natalia <215136933+Nana-04-art@users.noreply.github.com>
2026-09-08 10:02:14 +0200

    code/beesagono_be: implement Daily Puzzle Generator logic

---
+++ code/beesagono_be/src/main/java/com/beesagono/backend/repository/PuzzleWordRepository.java
@@ -0,0 +1,13 @@
+package com.beesagono.backend.repository;
+
+import com.beesagono.backend.entity.PuzzleWord;
+import com.beesagono.backend.entity.id.PuzzleWordId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PuzzleWordRepository extends JpaRepository<PuzzleWord, PuzzleWordId> {
+    Optional<PuzzleWord> findByPuzzleIdAndIdWord(String puzzleId, String word);
+
+    long countByIdPuzzleId(String puzzleId);
+}
