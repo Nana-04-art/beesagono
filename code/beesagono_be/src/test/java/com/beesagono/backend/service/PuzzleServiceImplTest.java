b09b7b814e7799d0d2639cbd192f424116053ca3
Natalia <215136933+Nana-04-art@users.noreply.github.com>
2026-09-09 16:10:14 +0200

    code/beesagono_be: update service, repository and controller test

--- code/beesagono_be/src/test/java/com/beesagono/backend/service/PuzzleServiceImplTest.java
+++ code/beesagono_be/src/test/java/com/beesagono/backend/service/PuzzleServiceImplTest.java
@@ -21,7 +21,10 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.never;
 
 @ExtendWith(MockitoExtension.class)
 class PuzzleServiceImplTest {
