2321c2ad6a2970a3bbc2a1b0a6b06918f3cd10b7
Natalia <215136933+Nana-04-art@users.noreply.github.com>
2026-09-14 17:15:45 +0200

    code/beesagono_be: update service, repository and controller test

--- code/beesagono_be/src/test/java/com/beesagono/backend/service/DictionaryServiceImplTest.java
+++ code/beesagono_be/src/test/java/com/beesagono/backend/service/DictionaryServiceImplTest.java
@@ -35,6 +35,7 @@ import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.never;
 
 @ExtendWith(MockitoExtension.class)
 class DictionaryServiceImplTest {
